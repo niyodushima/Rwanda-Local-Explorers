dc44eeca500a644976771b44f2d62b77e16d45ba
niyodushima <131856101+niyodushima@users.noreply.github.com>
2026-02-28 22:36:12 +0200

    Update Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -8,7 +8,10 @@ type ButtonProps = {
 
 export default function Button({ children, onClick, className }: ButtonProps) {
   return (
-    <button onClick={onClick} className={className}>
+    <button
+      onClick={onClick}
+      className={`bg-green-700 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-800 transition duration-300 ${className || ""}`}
+    >
       {children}
     </button>
   );
