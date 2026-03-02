87c62321cfa72d65e50dc0a1dbf666e734b535a0
niyodushima <131856101+niyodushima@users.noreply.github.com>
2026-03-03 00:02:11 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// ✅ Create root and render App
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found. Make sure index.html has a <div id='root'></div>");
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
